583ba4bdab1b169ee45de69d345a946edd0e9a47
Mamadou Diouf <papadiouf93@gmail.com>
2024-05-23 18:32:20 +0000

    first commit

---
+++ data/js/script.js
@@ -0,0 +1,9 @@
+const lienceappreant = document.getElementById('lienappreant');
+const lienclasse = document.getElementById('lienclasse');
+
+lienceappreant.addEventListener('click', () => {
+    alert('Cette application est réalisée par 2 étudiants');
+});
+lienclasse.addEventListener('click', () => {
+    alert('Cette application est réalisée par 2 étudiants');
+});
